9653b8cbb2f390ff00ecce414477de07c18a10cf
Bang MachDieu <mdba@netcompany.com>
2023-06-02 14:44:09 +0700

    add redux for cart state management

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-﻿import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
+﻿import React, { Component } from "react"
+import { Route, Routes } from "react-router-dom"
+import AppRoutes from "./AppRoutes"
+import { Layout } from "./components/Layout"
+import { Provider } from "react-redux"
+import store from "./store"
+
 //import a css for this
 
 export default class App extends Component {
-    static displayName = App.name;
-
-    render() {
-        return (
-            <Layout>
-                <Routes>
-                    {AppRoutes.map((route, index) => {
-                        const { element, ...rest } = route;
-                        return <Route key={index} {...rest} element={element} />;
-                    })}
-                </Routes>
-            </Layout>
-        );
-    }
+  static displayName = App.name
+  render() {
+    return (
+      <Provider store={store}>
+        <Layout>
+          <Routes>
+            {AppRoutes.map((route, index) => {
+              const { element, ...rest } = route
+              return <Route key={index} {...rest} element={element} />
+            })}
+          </Routes>
+        </Layout>
+      </Provider>
+    )
+  }
 }
